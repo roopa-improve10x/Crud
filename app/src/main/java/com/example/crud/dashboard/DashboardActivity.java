0d3444a8c02c3683210982bec541222e077f309f
roopa <roopa.peddinti@improve10x.com>
2022-12-16 11:19:41 +0530

    object names changed in dashboard activity

--- app/src/main/java/com/example/crud/dashboard/DashboardActivity.java
+++ app/src/main/java/com/example/crud/dashboard/DashboardActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
     //Todo : change the class name as DashboardItemsActivity
-    public ArrayList<DashboardItem> dashboardList; //= new ArrayList<>();
+    public ArrayList<DashboardItem> dashboardItems; //= new ArrayList<>();
     public RecyclerView dashboardRv;
     //Todo : change the recycler view name as dashboardItemsRv
     public DashboardAdapter dashboardAdapter;
@@ -29,26 +29,26 @@ public class DashboardActivity extends AppCompatActivity {
 
     public void setUpData() {
         //Todo: in setupmethod U should be in lower case
-        dashboardList = new ArrayList<>();
-        DashboardItem dashboard1 = new DashboardItem();
-        dashboard1.dashboardName = "Messages";
-        dashboard1.dashboardImageUrl = "https://images.macrumors.com/t/xhjvyZvcvdWDIi846TArb8MFzzk=/1600x/article-new/2020/07/messagesicon-200x200.png";
-        dashboardList.add(dashboard1);
+        dashboardItems = new ArrayList<>();
+        DashboardItem messages = new DashboardItem();
+        messages.dashboardName = "Messages";
+        messages.dashboardImageUrl = "https://images.macrumors.com/t/xhjvyZvcvdWDIi846TArb8MFzzk=/1600x/article-new/2020/07/messagesicon-200x200.png";
+        dashboardItems.add(messages);
 
-        DashboardItem dashboard2 = new DashboardItem();
-        dashboard2.dashboardImageUrl = "https://play-lh.googleusercontent.com/ZspusY01v4jlKuMuVikb0WjA-94plHCBvMJZVd3u59W_Kg1I4TPV3-HgB4VE1exhmfI";
-        dashboard2.dashboardName = "Templates";
-        dashboardList.add(dashboard2);
+        DashboardItem templates = new DashboardItem();
+        templates.dashboardImageUrl = "https://play-lh.googleusercontent.com/ZspusY01v4jlKuMuVikb0WjA-94plHCBvMJZVd3u59W_Kg1I4TPV3-HgB4VE1exhmfI";
+        templates.dashboardName = "Templates";
+        dashboardItems.add(templates);
 
-        DashboardItem dashboard3 = new DashboardItem();
-        dashboard3.dashboardImageUrl = "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/Franchise/hp-ss-min.png";
-        dashboard3.dashboardName = "Series";
-        dashboardList.add(dashboard3);
+        DashboardItem series = new DashboardItem();
+        series.dashboardImageUrl = "https://www.peacocktv.com/dam/growth/assets/seo/HarryPotter/Franchise/hp-ss-min.png";
+        series.dashboardName = "Series";
+        dashboardItems.add(series);
 
-        DashboardItem dashboard4 = new DashboardItem();
-        dashboard4.dashboardImageUrl = "https://nbcpalmsprings.com/wp-content/uploads/sites/8/2021/12/BEST-MOVIES-OF-2021.jpeg";
-        dashboard4.dashboardName = "Movies";
-        dashboardList.add(dashboard4);
+        DashboardItem movies = new DashboardItem();
+        movies.dashboardImageUrl = "https://nbcpalmsprings.com/wp-content/uploads/sites/8/2021/12/BEST-MOVIES-OF-2021.jpeg";
+        movies.dashboardName = "Movies";
+        dashboardItems.add(movies);
     }
 
     public void setDashboardRv() {
@@ -57,7 +57,7 @@ public class DashboardActivity extends AppCompatActivity {
         // Todo: create a separate method for id's
         dashboardRv.setLayoutManager(new LinearLayoutManager(this));
         dashboardAdapter = new DashboardAdapter();
-        dashboardAdapter.setData(dashboardList);
+        dashboardAdapter.setData(dashboardItems);
         dashboardRv.setAdapter(dashboardAdapter);
     }
 }
